e2dfb72e26f95cd84e5964ba163f723dec6588ad
John321Blacksmith <kuzmichevi321@gmail.com>
2026-01-23 14:01:52 +0300

    feat: added mod

--- cmd/playground/main.go
+++ cmd/playground/main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"fmt"
+	"playground/internal/scalability"
+)
+
+func main() {
+	var nums []int = []int{1, 7, 4, 9, 3, 6, 2}
+	fmt.Println(scalability.BubbleSort(nums))
+}
--- internal/scalability/algos.go
+++ internal/scalability/algos.go
@@ -1,5 +1,57 @@
 package scalability
 
-func BubbleSort(arr []int) []int {
+import (
+	"fmt"
+)
 
+// time complexity is O(n*2)
+func BubbleSort(arr []int) []int {
+	swapped := true
+
+	for swapped {
+		swapped = false
+
+		for i := 0; i < len(arr)-1; i++ {
+			if arr[i] > arr[i+1] {
+				arr[i], arr[i+1] = arr[i+1], arr[i]
+				swapped = true
+			}
+			fmt.Println(arr)
+
+		}
+	}
+	return arr
+}
+
+// Time complexity O(nlogn)
+func QuickSort(arr []int) []int {
+
+	// If the array has been divided to limits...
+	if len(arr) < 1 {
+		return arr
+	}
+
+	// Specify boundaries
+	left, right := 0, len(arr)-1
+
+	// Pick a pivot and move it to the end
+	pivot := (len(arr) - 1) / 2
+	arr[pivot], arr[right] = arr[right], arr[pivot]
+
+	// Partition
+	for i := range arr {
+		if arr[i] < arr[right] {
+			arr[i], arr[left] = arr[left], arr[i]
+			left++
+		}
+	}
+
+	// Putting the pivot in place
+	arr[left], arr[right] = arr[right], arr[left]
+
+	// Perform QuickSort recursively with sub-arrays
+	QuickSort(arr[:left])
+	QuickSort(arr[left+1:])
+
+	return arr
 }
